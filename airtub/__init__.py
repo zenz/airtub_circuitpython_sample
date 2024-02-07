2c10bd3ee8c10f687b300a847f331e607aec918a
Simon Hu <simonhu@airfit.cn>
2024-02-07 21:08:21 +0800

    按照pylint的建议修改部分代码

--- airtub/__init__.py
+++ airtub/__init__.py
@@ -1,33 +1,34 @@
-from adafruit_itertools import cycle
-from binascii import crc32, hexlify
+""" provide functions for data packing and unpacking """
+
 import struct
+from binascii import crc32, hexlify
+from adafruit_itertools import cycle
 
 
-# define xor encryption
 def xor_crypt(a: str, b: str):
+    """define xor encryption"""
     return "".join(chr(ord(x) ^ ord(y)) for x, y in zip(a, cycle(b)))
 
 
-# unpack data from udp receives
-def unpack_data(pack_data: bytes, size: int, secret: str):
-    """数据解码"""
+def unpack_data(raw_data: bytes, size: int, secret: str):
+    """unpack data from udp receives"""
     if size != 0:
-        msgtype = pack_data[0]
-        datalen = pack_data[1]
-        crc_bytes = struct.unpack("4B", pack_data[4:8])
+        msgtype = raw_data[0]
+        datalen = raw_data[1]
+        crc_bytes = struct.unpack("4B", raw_data[4:8])
         reversed_crc_bytes = bytearray()
         for i in range(len(crc_bytes) - 1, -1, -1):
             reversed_crc_bytes.append(crc_bytes[i])
         crc1 = hexlify(reversed_crc_bytes).decode()
-        crc2 = "{:08x}".format(crc32(pack_data[8 : datalen + 8]))
-        realdata = bytearray(pack_data[8 : datalen + 8])
+        crc2 = "{:08x}".format(crc32(raw_data[8 : datalen + 8]))
+        realdata = bytearray(raw_data[8 : datalen + 8])
         realdata = bytes(xor_crypt(realdata.decode("ascii"), secret), "ascii")
         return msgtype, datalen, realdata, crc1, crc2
     return 0, 0, b"", "", ""
 
 
-# prepare data for udp sends
 def pack_data(msgtype: int, message: str, secret: str):
+    """prepare data for udp sends"""
     len_num = len(message)
     crypt_data = xor_crypt(message, secret).encode("ascii")
     crc = crc32(crypt_data).to_bytes(4, "little")
